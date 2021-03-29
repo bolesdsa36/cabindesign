69599b88c28e17535575c4d18992e281aa709269
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-03-30 09:32:02 +1300

    Next Steps to adding bulma and deleting unnecessary files.
    I removed the counter.svelte file and deleted contents from the index files.
    I incorrectly inputted the bulma file because I typed "npm" and not "npx" so the computer could not understand my command. Next I will remove the bulma files and begin begin creating my inital routes and components files.

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,8 +1,12 @@
+const sveltePreprocess = require("svelte-preprocess");
 const node = require('@sveltejs/adapter-node');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+	preprocess: [
+		sveltePreprocess(),
+	],
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
