2af7f76100452e702111ee7110e4542146aca7b4
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-07 13:37:18 +1200

    Fixed svelte error
    Had trouble with the svelte errors so had to fix this by resetting files in .json.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,19 +4,35 @@
 	import Footer from "$lib/Footer.svelte";
 
 </script>
+
 <Nav />
+
 <Header />
 
 
 
 <main>
-	<h1>
-        
-    </h1>
-
+dfghjk
 </main>
 
-<style>
 
+<Footer />
+
+<style>
+		/* LAYOUT */
+  :global(Header) { grid-area: h; }
+  :global(Nav)    { grid-area: n; }
+  :global(main)   { grid-area: m; }
+  :global(Footer) { grid-area: f; }
+
+  :global(#svelte) {
+    display: grid;
+    grid: " n   n   " auto
+          " h   h   " auto
+          " m   m   " auto
+          " f   f   " auto
+          / 1fr 3fr ;
+  }
 
 </style>
+
