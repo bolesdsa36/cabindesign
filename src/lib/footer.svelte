3e67a339e9c29396313fb44ab4e653d3c0fc403a
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-12 21:59:37 +1200

    Finally sorted out why the layout of my sections and columns wouldnt work. Also created and added a footer featured at the bottom of my page.
    I had to look at my code more accurately to see that the layout of my divs wern't correct and my top section didnt match my bottom section.

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,9 +1,18 @@
 <footer>
-
-
-</footer>
-
-<style>
-
-    
-</style>
+    <p>
+      &copy; Sarah Mercedes 2021
+    </p>
+  </footer>
+  
+  <style>
+    footer {
+      height: 5em;
+      background-color: #D8AA96;
+    }
+  
+    p {
+      text-align: center;
+      vertical-align: middle;
+      color: white;
+    }
+  </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,42 +10,42 @@
 
 <main>
     <section class="section">
-    <div class="columns">
-        <div class="column">
-            <a href="design-heritage">
-                <img src="images/image12.jpg" alt="graphics-poster"/>
-            </a>
-        </div>
-        <div class="column">
+        <div class="columns">
+            <div class="column">
+                <a href="design-heritage">
+                    <img src="images/image12.jpg" alt="graphics-poster"/>
+                </a>
+            </div>
             
-                Click on the image to the left which will redirect you to
-                        the Design Heritage Page. You will need to complete all
-                        tasks to be able to advance to the next section. Please
-                        complete all tasks to a high standard.
-      </div>
+            <div class="column">
+            <p>
+                Click on the image to the left which will redirect you to the
+                Design Heritage Page. You will need to complete all tasks to be
+                able to advance to the next section. Please complete all tasks
+                to a high standard.
+            </p>
+        </div>
     </section>
-    <section class="section">
-        <div class="column-left">
-        Click on the image to the left which will redirect you to
-                    the Visual Communication Page. You will need to complete all
-                    tasks to be able to advance to the next section. Please
-                    complete all tasks to a high standard.
-  
-</div>
-</section>
-    
+
     <section class="section">
         <div class="columns">
-            <div class="column-is-right-aligned">
+            <div class="column">
+                <p>
+                    Click on the image to the right which will redirect you to the
+                    Visual Communications Page. You will need to complete all tasks to be
+                    able to advance to the next section. Please complete all tasks
+                    to a high standard.
+                </p>
+            </div>
+            <div class="column">
                 <a href="visual-communication">
-                <img src="images/image8.jpg" alt="bird-houses"/>
+                    <img src="images/image8.jpg" alt="birds-house-poster"/>
                 </a>
-
-   <div>
-</section>
-    
-
+            </div>
+            
+            
+        </div>
+    </section>
 </main>
 
 <Footer />
-
