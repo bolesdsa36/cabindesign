63faaae3d49539c2683c49f8d4bf2a46c86a5b2e
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-12 20:14:45 +1200

    Inputted image 2 and have reformatted my text because of error codes.
    My code kept coming up with bulma error messages so I had to re adjust my text using specifically bulma code. This meant i couldnt use any of my own personalisation into the code because often error codes do appear when i do this.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,51 @@
 <script>
-	import Header from "$lib/Header.svelte";
-	import Nav from "$lib/Nav.svelte";
-	import Footer from "$lib/Footer.svelte";
-
+    import Header from "$lib/Header.svelte";
+    import Nav from "$lib/Nav.svelte";
+    import Footer from "$lib/Footer.svelte";
 </script>
 
 <Nav />
 
 <Header />
 
-
-
 <main>
-  <section>
+    <section class="section">
+    <div class="columns">
+        <div class="column">
+            <a href="design-heritage">
+                <img src="images/image12.jpg" alt="graphics-poster"/>
+            </a>
+        </div>
+        <div class="column">
+            
+                Click on the image to the left which will redirect you to
+                        the Design Heritage Page. You will need to complete all
+                        tasks to be able to advance to the next section. Please
+                        complete all tasks to a high standard.
+      </div>
+    </section>
+    <section class="section">
+        <div class="column-left">
+        Click on the image to the left which will redirect you to
+                    the Visual Communication Page. You will need to complete all
+                    tasks to be able to advance to the next section. Please
+                    complete all tasks to a high standard.
+  
+</div>
+</section>
+    
+    <section class="section">
+        <div class="columns">
+            <div class="column-is-right-aligned">
+                <a href="visual-communication">
+                <img src="images/image8.jpg" alt="bird-houses"/>
+                </a>
+
+   <div>
+</section>
+    
 
 </main>
 
-
 <Footer />
-
-<style>
-		/* LAYOUT */
-  :global(Header) { grid-area: h; }
-  :global(Nav)    { grid-area: n; }
-  :global(main)   { grid-area: m; }
-  :global(Footer) { grid-area: f; }
-
-  :global(#svelte) {
-    display: grid;
-    grid: " n   n   " auto
-          " h   h   " auto
-          " m   m   " auto
-          " f   f   " auto
-          / 1fr ;
-  }
-
-</style>
 
