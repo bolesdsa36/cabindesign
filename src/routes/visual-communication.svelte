cf359ebef7252206f52a73d068b900ba1629d9c1
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-15 10:26:09 +1200

    Updated the list using the provided text for task 4 on the provided resource
    I changed the copied text to the text that was provide. I also changed the image so it corrosponds to the text.

--- src/routes/visual-communication.svelte
+++ src/routes/visual-communication.svelte
@@ -53,37 +53,50 @@
                     <ol type="a">
                         <li>What does 1:2 mean?</li>
                         <li>Using 1:1 what size would you draw 100mm at?</li>
-                        <li>Using 1:20 what size would you draw 100mm at? </li>
+                        <li>Using 1:20 what size would you draw 100mm at?</li>
                         <li>Using 1:50 what size would you draw 100mm at?</li>
                         <li>How do you work out scale? Explain this</li>
-                        <li>What unit of measurement must all dimensions be in?</li>
+                        <li>
+                            What unit of measurement must all dimensions be in?
+                        </li>
                     </ol>
                 </div>
-            
-            
-            
             </div>
-            <div class="column">
-                
-            </div>
+            <div class="column" />
         </div>
     </section>
 
     <section class="section">
         <div class="columns">
             <div class="column">
-                <h1 class="has-text-weight-bold">2. Perspective Bird Houses</h1>
-                <p>Draw the bird houses. Add shadows and textures</p>
-            </div>
-            <div class="column">
-                <a href="visual-communication">
-                    <img src="images/image8.jpg" alt="birds-house-poster" />
+                <a href="Design-heritage">
+                    <img src="images/image9.jpg" alt="graphics-room" />
                 </a>
+            </div>
+
+            <div class="column">
+                <h1 class="has-text-weight-bold">4. Architectural Symbols</h1>
+                <p>
+                    Draw up a plan view of a room that will show the following.
+                    Label each neatly in 3mm caps writing. You can adapt the
+                    drawing below or do your own. Think about colour and
+                    texture.
+                </p>
+                <div class="content">
+                    <ol type="a">
+                        <li>Double doors</li>
+                        <li>3 windows</li>
+                        <li>Walls</li>
+                        <li>A sink, a toilet and bath</li>
+                        <li>Seating</li>
+                        <li>Add a north symbol used for architecture</li>
+                        <li>Tiles</li>
+                        <li>Wooden Flooring</li>
+                    </ol>
+                </div>
             </div>
         </div>
     </section>
-
-
 </main>
 
 <Footer />
