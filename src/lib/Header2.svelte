71f620dd11e40c4cc985b09560ef9833362f8a89
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-15 09:28:54 +1200

    Copied previous heading code
    I copied previous heading 1 code so i could easily paste the most accurate code into the new file and i will have to make minimal changes as the format is already matching the other pages. The only thing i now need to change is the Design Heritage text to Visual Communication text.

---
+++ src/lib/Header2.svelte
@@ -0,0 +1,15 @@
+<section class="hero is-link is-fullheight-with-navbar">
+    <div class="hero-body">
+      <p class="title">
+        Design Heritage Homework Task
+      </p>
+    </div>
+  </section>
+
+  <style>
+p{
+
+  font-family: 'Kiwi Maru', serif;
+}
+
+  </style>
