7735803189e81e20fb3735c6e7476eefcd2b07b4
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-15 09:47:12 +1200

    I added another lot of column and section text because there are more tasks.
    There are more tasks i need to display therefor in need another lot of section and column code so these tasks can be displayed.

--- src/routes/visual-communication.svelte
+++ src/routes/visual-communication.svelte
@@ -44,6 +44,42 @@
             </div>
         </div>
     </section>
+
+    <section class="section">
+        <div class="columns">
+            <div class="column">
+                <a href="Design-heritage">
+                    <img src="images/image10.jpg" alt="graphics-poster1" />
+                </a>
+            </div>
+
+            <div class="column">
+                <h1 class="has-text-weight-bold">1. Perspective</h1>
+                <p>
+                    Use the box examples on this sheet and create on your own
+                    page. Start by putting in a line for the horizon and the
+                    VP’s then the front side of each box. Then take the sides
+                    back to the vanishing points. If you get stuck draw the
+                    example. Place boxes in all different locations and make
+                    them different sizes.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="columns">
+            <div class="column">
+                <h1 class="has-text-weight-bold">1. Perspective</h1>
+                <p>Draw the bird houses. Add shadows and textures</p>
+            </div>
+            <div class="column">
+                <a href="visual-communication">
+                    <img src="images/image8.jpg" alt="birds-house-poster" />
+                </a>
+            </div>
+        </div>
+    </section>
 </main>
 
 <Footer />
