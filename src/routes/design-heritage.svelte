bc89f6a1acb639a7a58bc17057691f6c9a387308
bolesdsa36 <78567670+bolesdsa36@users.noreply.github.com>
2021-04-15 14:00:25 +1200

    Changing weights and colours of fonts
    I had to change the colours of the fonts and weight it to bold to keep it consistant across all of my pages.

--- src/routes/design-heritage.svelte
+++ src/routes/design-heritage.svelte
@@ -33,48 +33,58 @@
         </div>
     </section>
    
+
+        <section class="section">
+            <div class="columns">
+                <div class="column">
+                    <img src="images/image6.png" alt="house" />
+                </div>
     
-    <h2>Creative Challenge...</h2>
-    <p>
-        Your presentation should reflect the style of the architect you have
+                <div class="column">
+                    <h1 class="has-text-weight-bold">Creative Challenge...</h1>
+                    <p>
+                        Your presentation should reflect the style of the architect you have
         studied (shapes colours textures etc.).
-    </p>
+                    </p>
+                    <h2>Answer...</h2>
+                    <h3 class="has-text-weight-bold">
+                        What are the key features of their work?- aesthetic (looks) and
+                        functional (working)
+                    </h3>
+                    <div class="content">
+                        <ul>
+                            <li>Include examples of their work and a few notes</li>
+                        </ul>
+                        <h3>
+                            What context do they work within?- what cultural/historical/society or
+                            technological things influence their work?
+                        </h3>
+                        <div class="content">
+                            <ul>
+                                <li>
+                                    Do they have a design philosophy?- what do they try and do or
+                                    think about design
+                                </li>
+                                <li>How long have they been designing?</li>
+                                <li>What are the main materials their buildings are made from?</li>
+                                <li>Are there any interesting facts you have found about them?</li>
+                                <li>Include a quote that they have said</li>
+                            </ul>
+                        </div>
+                
+                
+                    </div>
+            </div>
+        </section>
 
-    <h2>Answer...</h2>
-    <h3>
-        What are the key features of their work?- aesthetic (looks) and
-        functional (working)
-    </h3>
-    <div class="content">
-        <ul>
-            <li>Include examples of their work and a few notes</li>
-        </ul>
-    </div>
-    <h3>
-        What context do they work within?- what cultural/historical/society or
-        technological things influence their work?
-    </h3>
-    <div class="content">
-        <ul>
-            <li>
-                Do they have a design philosophy?- what do they try and do or
-                think about design
-            </li>
-            <li>How long have they been designing?</li>
-            <li>What are the main materials their buildings are made from?</li>
-            <li>Are there any interesting facts you have found about them?</li>
-            <li>Include a quote that they have said</li>
-        </ul>
-    </div>
-
-    
 </main>
 
 <Footer />
 
 <style>
     h2 {
-        font-size: 1.5em;
+        
+        font-size: 1em;
     }
     main {
         padding-left: 1em;
@@ -84,6 +94,8 @@
     }
     h3 {
         color:#f7b1ab;
+        font-size: 1em;
+        text-decoration: double;
     }
     p {
         padding-bottom: 1em;
